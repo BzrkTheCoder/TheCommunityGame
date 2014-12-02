e4517818490040579d432dc5478735017e92c788
BzrkTheCoder (Indie Dev) <bzrkthecoder@gmail.com>
2014-12-02 20:00:58 +0000

    Update Game.java

--- src/com/community/game/Game.java
+++ src/com/community/game/Game.java
@@ -1,9 +1,12 @@
-package com.bzrkthecoder.javagame;
+package com.bzrkthecoder.thecommunitygame;
 
 import java.awt.Canvas;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
 
 import javax.swing.JFrame;
 
@@ -11,12 +14,15 @@ public class Game extends Canvas implements Runnable {
 	private static final long serialVersionUID = 1L;
 	
 	public static int width = 300;
-	public static int height = width * 16 / 9;
+	public static int height = width / 16 * 9;
 	public static int scale = 3;
 	
 	private Thread thread;
 	private JFrame frame;
 	private boolean running = false;
+	
+	private BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+	private int[] pixels = ((DataBufferInt)image.getRaster().getDataBuffer()).getData();
 	
 	public Game() {
 		Dimension size = new Dimension(width * scale, height * scale);
@@ -50,11 +56,11 @@ public class Game extends Canvas implements Runnable {
 	}
 	
 	public void render() {
-	BufferStrategy bs = getBufferStrategy();
-	if (bs == null) {
-		createBufferStrategy(3);
-		return;
-	}
+		BufferStrategy bs = getBufferStrategy();
+		if (bs == null) {
+			createBufferStrategy(3);
+			return;
+		}
 		
 		Graphics g = bs.getDrawGraphics();
 		g.setColor(Color.BLACK);
@@ -62,7 +68,6 @@ public class Game extends Canvas implements Runnable {
 		g.dispose();
 		bs.show();
 	}
-	
 	
 	public static void main(String[] args) {
 		Game game = new Game();
